18a95869ed87869527470964c3e11d53b78c8185
Victor <vbmachado123@gmail.com>
2020-06-02 18:47:10 -0300

    Recebendo notificacoes e iniciando conexao via service

--- app/src/main/java/services/BluetoothService.java
+++ app/src/main/java/services/BluetoothService.java
@@ -5,6 +5,7 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -13,6 +14,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
+import android.os.Parcelable;
 import android.os.PowerManager;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -31,84 +34,93 @@ import java.util.concurrent.Executor;
 import gen.FuncoesSOS;
 
 /* Responsavel por iniciar e tornar publica a conexão com o dispositivo */
-public class BluetoothService extends IntentService {
+public class BluetoothService extends Service {
     private PowerManager.WakeLock lock;
 
     private static String MAC = null;
     private AsciiCommander commander;
-    private BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();;
+    private BluetoothAdapter adapter = null;
     private BluetoothDevice device = null;
     private BluetoothSocket socket = null;
 
     private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-    public BluetoothService() {
-        super("BluetoothService");
-    }
+    public  BluetoothService(){}
+    public boolean conexao = false;
 
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public BluetoothService(String name) {
-        super(name);
-    }
-
-    public BluetoothService(Context context) {
-        super("BluetoothService");
-    }
-
-    @SuppressLint("InvalidWakeLockTag")
+    @Nullable
     @Override
-    protected void onHandleIntent(Intent intent) {
+    public IBinder onBind(Intent intent) {
+        MAC =(String) intent.getExtras().get("address");
+        return null;
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        commander = new AsciiCommander(getApplicationContext());
         adapter = BluetoothAdapter.getDefaultAdapter();
-        try {
-            /* device ta retornando NULLPOINTER */
-            MAC = (String) intent.getExtras().get("address");
-            // MAC = "88:6B:0F:F0:52:78";
-            /*adapter = BluetoothAdapter.getDefaultAdapter();*/
-            device = adapter.getRemoteDevice(MAC);
-            socket = device.createRfcommSocketToServiceRecord(uuid);
+        MAC =(String) intent.getExtras().get("address");
+        device = adapter.getRemoteDevice(MAC);
+        commander.connect(device);
 
-            if(device != null) {
-                commander.connect(device);
-            }
+        conexao = true;
+        enviarDadosActivity();
 
-            if(commander.hasConnectedSuccessfully()) {
-                Log.i("Conexao_Bluetooth", "conexao bem sucedida");
-            }
+        startForeground(FuncoesSOS.NOTIFICATION_ID_PADRAO, FuncoesSOS.sendNotificationPadrao(getApplicationContext(), device.getName()));
+        return START_STICKY;
+    }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e("Conexao_Bluetooth", "erro IOException: " + e);
-        } catch (Exception e){
-            e.printStackTrace();
-            Log.e("Conexao_Bluetooth", "erro exception: " + e);
-        }
-
-        startForeground(FuncoesSOS.NOTIFICATION_ID_PADRAO, FuncoesSOS.sendNotificationPadrao(getApplicationContext()));
-
-
-        lock = ((PowerManager) getSystemService(
-                Context.POWER_SERVICE)).newWakeLock(
-                PowerManager.PARTIAL_WAKE_LOCK
-                        | PowerManager.ACQUIRE_CAUSES_WAKEUP, "BluetoothService");
-        lock.acquire();
-
+    private void enviarDadosActivity() { /* Teste de envio para as telas */
+        Intent enviar = new Intent();
+        enviar.setAction("GET_CONEXAO");
+        enviar.putExtra( "conexao",conexao);
+      /*  enviar.putExtra( "commander", (Parcelable) commander);
+        enviar.putExtra( "device", (Parcelable) device);*/
+        sendBroadcast(enviar);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        commander = new AsciiCommander(getApplicationContext());
         adapter = BluetoothAdapter.getDefaultAdapter();
+
         if (Build.VERSION.SDK_INT >= 26)
-            startForeground(FuncoesSOS.NOTIFICATION_ID_PADRAO, FuncoesSOS.sendNotificationPadrao(getApplicationContext()));
+            startForeground(FuncoesSOS.NOTIFICATION_ID_PADRAO, FuncoesSOS.sendNotificationPadrao(getApplicationContext(), device.getName()));
     }
 
     public void onDestroy() {
         super.onDestroy();
+        commander.disconnect();
         lock.release();
+    }
+
+    public boolean isConexao() {
+        return conexao;
+    }
+
+    public void setConexao(boolean conexao) {
+        this.conexao = conexao;
+    }
+
+    public static String getMAC() {
+        return MAC;
+    }
+
+    public AsciiCommander getCommander() {
+        return commander;
+    }
+
+    public BluetoothAdapter getAdapter() {
+        return adapter;
+    }
+
+    public BluetoothDevice getDevice() {
+        return device;
+    }
+
+    public BluetoothSocket getSocket() {
+        return socket;
     }
 }
